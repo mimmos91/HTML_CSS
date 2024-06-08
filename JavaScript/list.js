e7bb9e0f8903183f75ae7a429fa1a7567c2ff3a0
mimmos91 <mimmos@hanmail.net>
2024-06-08 23:19:48 +0900

    sort 끝

--- JavaScript/list.js
+++ JavaScript/list.js
@@ -1,118 +1,148 @@
-
 var products = [{
-                id: 0,
-                price: 70000,
-                title: 'Blossom Dress'
+        id: 0,
+        price: 70000,
+        title: 'Blossom Dress'
             },
-            {
-                id: 1,
-                price: 50000,
-                title: 'Springfield Shirt'
+    {
+        id: 1,
+        price: 50000,
+        title: 'Springfield Shirt'
             },
-            {
-                id: 2,
-                price: 60000,
-                title: 'Black Monastery'
+    {
+        id: 2,
+        price: 60000,
+        title: 'Black Monastery'
             }
         ];
 
 
-        function 출력(manyPriducts) {
-            console.log(manyPriducts);
+function 출력(manyPriducts) {
+    console.log(manyPriducts);
 
-            for (var i = 0; i < manyPriducts.length; i++) {
-                var lists = `<div class="col-sm-4">
+    for (var i = 0; i < manyPriducts.length; i++) {
+        var lists = `<div class="col-sm-4">
         <img src="https://via.placeholder.com/600"class="w-100">
         <h5>${manyPriducts[i].title}</h5>
         <p>가격 : ${manyPriducts[i].price}</p>
   </div>
 </div>`
-                $('.row').append(lists);
-                //            $('.card-body h5').eq(i).html(products[i].title);
-                //            $('.card-body p').eq(i).html('가격: ' + products[i].price);
-
-            }
-
-
-
-        }
-
-
-
-
-        출력(products);
-        let count = 0;
-
-        $('.plus').on('click', function() {
-
-            if (count == 0) {
-                $.get('https://codingapple1.github.io/js/more1.json').done(function(data) {
-
-                    출력(data);
-
-                })
-                count++;
-            }else if(count==1){
-                 $.get('https://codingapple1.github.io/js/more2.json').done(function(data) {
-
-                    출력(data);
-
-                })
-                count++;
-                $('.plus').addClass('form-hide');
-
-
-            }
-
-        })
-
-
-    function 합쳐라(products){
-
-        $.get('https://codingapple1.github.io/js/more1.json').done(function(data) {
-
-                    products = products.concat(data);
-
-
-            $.get('https://codingapple1.github.io/js/more2.json').done(function(data) {
-
-                    products = products.concat(data);
-                console.log(products);
-                return products;
-
-                })
-
-                })
-
+        $('.row').append(lists);
+        //            $('.card-body h5').eq(i).html(products[i].title);
+        //            $('.card-body p').eq(i).html('가격: ' + products[i].price);
 
     }
 
 
-function 지우기(){
 
-    $('.row').remove;
 }
 
 
 
 
-    $('.cheap').on('click',function(){
+출력(products);
+let count = 0;
 
-        var under6 = 합쳐라(products);
-        under6.sort(function(a,b){
+$('.plus').on('click', function () {
 
-        if(a.price<=6000){
+    if (count == 0) {
+        $.get('https://codingapple1.github.io/js/more1.json').done(function (data) {
 
-            return a-b;
-        }else{
+            출력(data);
 
-        }
+        })
+        count++;
+    } else if (count == 1) {
+        $.get('https://codingapple1.github.io/js/more2.json').done(function (data) {
 
-            console.log('under6'+under6);
+            출력(data);
 
-        });
+        })
+        count++;
+        $('.plus').addClass('form-hide');
 
+
+    }
+
+})
+
+
+$('.all').on('click', function () {
+
+    $('.row').html('');
+
+    var   allproducts =[];
+    allproducts=allproducts.concat(products);
+
+    $.get('https://codingapple1.github.io/js/more1.json').done(function (data) {
+
+        allproducts = allproducts.concat(data);
+
+
+        $.get('https://codingapple1.github.io/js/more2.json').done(function (data) {
+
+            allproducts = allproducts.concat(data);
+                $('.row').html('');
+            출력(allproducts);
+            $('.plus').addClass('form-hide');
+
+        })
 
     })
 
 
+})
+
+
+
+
+
+$('.cheap').on('click', function () {
+
+    var allproduct =[];
+    var under6 = [];
+        $('.row').html('');
+    allproduct=allproduct.concat(products);
+    $.get('https://codingapple1.github.io/js/more1.json').done(function (data) {
+
+        allproduct = allproduct.concat(data);
+
+
+        $.get('https://codingapple1.github.io/js/more2.json').done(function (data) {
+
+            allproduct = allproduct.concat(data);
+
+            allproduct.forEach(function(a) {
+
+                console.log(a.price);
+                console.log(parseInt(a.price) < 60000);
+
+
+                if (parseInt(a.price) < 60000) {
+
+                    under6.push(a);
+
+                }
+
+
+
+
+            });
+
+            $('.plus').addClass('form-hide');
+            출력(under6);
+
+
+        })
+
+    })
+
+
+})
+
+$('.back').on('click',function(){
+     $('.row').html('');
+    출력(products);
+    count=0;
+    $('.plus').addClass('form-show');
+
+})
